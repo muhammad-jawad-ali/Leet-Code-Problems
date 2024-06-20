500db2a4badf7cba4506b37c2bba73e23363b831
Muhammad Jawad Ali <167105720+jimjawad57@users.noreply.github.com>
2024-06-20 18:34:28 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,36 +1,12 @@
 class Solution {
 public:
-    bool canPlaceBalls(std::vector<int>& position, int m, int min_force) {
-        int count = 1; 
-        int last_position = position[0];
-        
-        for (int i = 1; i < position.size(); ++i) {
-            if (position[i] - last_position >= min_force) {
-                count++;
-                last_position = position[i];
-                if (count == m) return true;
-            }
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(),position.end());
+        if(m==2){
+            return *max_element(position.begin(), position.end()) - *min_element(position.begin(), position.end());
         }
-        return false;
-    }
-    
-    int maxDistance(std::vector<int>& position, int m) {
-        std::sort(position.begin(), position.end());
-        
-        int left = 1;
-        int right = position.back() - position.front();
-        int result = 0;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canPlaceBalls(position, m, mid)) {
-                result = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+        else if {
+            position.size()/m <= 2;
         }
-        
-        return result;
     }
 };
