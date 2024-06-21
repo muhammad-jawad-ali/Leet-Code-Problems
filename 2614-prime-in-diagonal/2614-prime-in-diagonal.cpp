5756251da2475a5811dc78a708f14b8074e1fa74
Muhammad Jawad Ali <167105720+jimjawad57@users.noreply.github.com>
2024-06-21 22:39:24 +0500

    Time: 90 ms (46.56%), Space: 38.2 MB (69.22%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
@@ -23,15 +23,13 @@ public:
 
         return max;
     }
-    bool isPrime(int n)
-    {
-    if (n <= 1)
-        return false;
- 
-    for (int i = 2; i <= n / 2; i++)
-        if (n % i == 0)
-            return false;
- 
-    return true;
+    bool isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
     }
 };
