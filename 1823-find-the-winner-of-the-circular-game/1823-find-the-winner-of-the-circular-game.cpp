bbcb0d3be750309360577b569756c114160f7036
Muhammad Jawad Ali <167105720+muhammad-jawad-ali@users.noreply.github.com>
2024-07-08 09:21:08 +0500

    Time: 2 ms (75.45%), Space: 7.2 MB (76.96%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,48 +1,10 @@
-#include <vector>
-#include <iostream>
-
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        std::vector<int> arr;
-        for(int i = 0; i < n; i++){
-            arr.push_back(i + 1);
+        int winner = 0;
+        for (int i = 1; i <= n; i++) {
+            winner = (winner + k) % i;
         }
-        
-        int index = 0;
-        for(int j = 1; j < n; j++){
-            int count = 0;
-            while(count < k){
-                if(arr[index] != -1){
-                    count++;
-                }
-                if(count == k) break;  
-                index++;
-                if(index == n){
-                    index = 0;
-                }
-            }
-            arr[index] = -1;
-
-            while(arr[index] == -1){  
-                index++;
-                if(index == n){
-                    index = 0;
-                }
-            }
-
-            for(int k = 0; k < n; k++){
-                cout << arr[k] << " ";
-            }
-            cout << "---" << j << endl;
-        }
-
-        for(int k = 0; k < n; k++){
-            if(arr[k] != -1){
-                return arr[k];
-            }
-        }
-
-        return 0;
+        return winner + 1;
     }
 };
