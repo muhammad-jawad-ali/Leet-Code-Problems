aad5310d16c4642bd93d5897029d4c24cac4035b
Muhammad Jawad Ali <167105720+muhammad-jawad-ali@users.noreply.github.com>
2024-09-27 19:01:50 +0500

    Time: 0 ms (100%), Space: 9.1 MB (86.87%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> list;
+
+        sort(nums.begin(),nums.end());
+        do{
+            list.push_back(nums);
+        }
+        while(next_permutation(nums.begin(),nums.end()));
+
+        return list;
+    }
+};
